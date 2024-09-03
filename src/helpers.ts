a30508a9cd685e63ab55c41b4fe0b59f84efbed1
Ngoding Bang <ngodingbang@gmail.com>
2024-09-03 17:45:46 +0700

    feat: change generate filename function using slug term

--- src/helpers.ts
+++ src/helpers.ts
@@ -41,7 +41,15 @@ export async function getCoverLink(
 }
 
 export function getFileName(title: string): string {
-  return title.replaceAll(" ", "-").replace(/--+/g, "-") + ".md";
+  return (
+    title
+      .normalize("NFD") // split an accented letter in the base letter and the acent
+      .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+      .replace(/\s+/g, "-") + ".md"
+  );
 }
 
 export function getFileFullName(page: PageObjectResponse): string {
